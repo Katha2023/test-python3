84fa177cf8cccd4f0545107f7306b8149d24dc7b
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-08-14 05:18:52 +0530

    Update runner_script.sh

--- runner_script.sh
+++ runner_script.sh
@@ -7,15 +7,15 @@ python3 -m venv myenv
 source myenv/bin/activate
 pip3 install -r requirements.txt
 
-mkdir results
-mkdir plots
+#mkdir results
+#mkdir plots
 
-taus=$(python3 -c "import numpy as np; print(' '.join(str(t) for t in np.array([350, 400])))")
+#taus=$(python3 -c "import numpy as np; print(' '.join(str(t) for t in np.array([350, 400])))")
 #taus=$(python3 -c "import numpy as np; print(' '.join(str(t) for t in np.linspace(300, 400, 25)))")
-for tau in $taus; do
-    echo "Running tau=$tau..."
-    python3 square_wave_single_tau.py "$tau"
-done
+#for tau in $taus; do
+    #echo "Running tau=$tau..."
+    #python3 square_wave_single_tau.py "$tau"
+#done
 
 python3 plotter.py 10000
 
