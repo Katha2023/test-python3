80f1182228bf5e6617e787c1d88024a4548545e7
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-08-14 04:15:49 +0530

    Update runner_script.sh

--- runner_script.sh
+++ runner_script.sh
@@ -3,6 +3,11 @@ sudo apt install python3-pip python3-venv
 
 python3 -m venv myenv
 source myenv/bin/activate
-
 pip3 install -r requirements
-python3 square_wave_single_tau.py 350
+
+taus=$(python3 -c "import numpy as np; print(' '.join(str(t) for t in np.array([350, 400])))")
+#taus=$(python3 -c "import numpy as np; print(' '.join(str(t) for t in np.linspace(300, 400, 25)))")
+for tau in $taus; do
+    echo "Running tau=$tau..."
+    python3 square_wave_single_tau.py "$tau"
+done
