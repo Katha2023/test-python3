747d45617f81c984962e7e6df31a0e921a1b3e72
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-08-14 03:43:11 +0530

    Update square_wave_opt.py

--- square_wave_opt.py
+++ square_wave_opt.py
@@ -5,44 +5,44 @@ from multiprocessing import Pool, cpu_count
 import time
 from tqdm import tqdm
 
-wc = 3.4375 * 2 * np.pi
-wa_bar = 2.137 * 2 * np.pi
-A = 0.3*2* np.pi
-g  = 0.05 * 2 * np.pi
-kappa = 0.0004
-N = 5
-
-tlist = np.linspace(0, 80000, 1000)
-wm_vals = np.linspace(1.28,1.32, 100) * 2 * np.pi
-tau_vals = np.linspace(300, 400, 25)
-tau_vals = np.array([350, 400])
-
-a = tensor(destroy(N), qeye(2))
-sm = tensor(qeye(N), destroy(2))
-adag_a = a.dag() * a
-sigp_sig = sm.dag() * sm
-psi0 = tensor(basis(N, 0), basis(2, 1))
-rho0 = ket2dm(psi0)
-
-c_ops = []
-if kappa > 0.0:
-    c_ops.append(np.sqrt(kappa) * a)
-
-H0 = wc * adag_a + g * (a.dag()*sm + a*sm.dag())
-e_ops = [sigp_sig]
-opts = {'nsteps': 50000}
-
-td_expr = 'wa_bar + A*cos(wm*t) if (t % T) < tau else wa_bar'
+start_time = time.time()
 
 pop_matrices = []        # stores time slice for each τ
 full_dynamics = []       # stores full dynamics for each τ
 
-start_time = time.time()
-
-desired_time = 10000
-t_index = np.argmin(np.abs(tlist - desired_time))
-
 for tau in tau_vals:
+    wc = 3.4375 * 2 * np.pi
+    wa_bar = 2.137 * 2 * np.pi
+    A = 0.3*2* np.pi
+    g  = 0.05 * 2 * np.pi
+    kappa = 0.0004
+    N = 5
+    
+    tlist = np.linspace(0, 80000, 1000)
+    wm_vals = np.linspace(1.28,1.32, 100) * 2 * np.pi
+    tau_vals = np.linspace(300, 400, 25)
+    tau_vals = np.array([350, 400])
+    
+    a = tensor(destroy(N), qeye(2))
+    sm = tensor(qeye(N), destroy(2))
+    adag_a = a.dag() * a
+    sigp_sig = sm.dag() * sm
+    psi0 = tensor(basis(N, 0), basis(2, 1))
+    rho0 = ket2dm(psi0)
+    
+    c_ops = []
+    if kappa > 0.0:
+        c_ops.append(np.sqrt(kappa) * a)
+    
+    H0 = wc * adag_a + g * (a.dag()*sm + a*sm.dag())
+    e_ops = [sigp_sig]
+    opts = {'nsteps': 50000}
+    
+    td_expr = 'wa_bar + A*cos(wm*t) if (t % T) < tau else wa_bar'
+    
+    desired_time = 10000
+    t_index = np.argmin(np.abs(tlist - desired_time))
+    
     H_td = [H0, [sigp_sig, td_expr]]
 
     def solve_for_wm(wm):
