2283abcd8dc07cf362fd9c87d70452cd5971fa08
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-08-13 10:44:15 +0530

    Update activate_env.sh

--- activate_env.sh
+++ activate_env.sh
@@ -1,3 +1,8 @@
-sudo apt install python3-venv
+sudo apt update
+sudo apt install python3-pip python3-venv
+
 python3 -m venv myenv
 source myenv/bin/activate
+
+pip3 install -r requirements
+python3 square_wave_opt.py
