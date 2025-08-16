3b24bec30a019ef716c6e181aa730358738be5bc
Katha2023 <144071067+Katha2023@users.noreply.github.com>
2025-08-15 20:34:12 -0400

    Update runner_script.sh

--- runner_script.sh
+++ runner_script.sh
@@ -11,7 +11,7 @@ mkdir results
 mkdir plots
 
 #taus=$(python3 -c "import numpy as np; print(' '.join(str(t) for t in np.array([350, 400])))")
-taus=$(python3 -c "print(' '.join(str(t) for t in range(100, 201, 4)))")
+taus=$(python3 -c "print(' '.join(str(t) for t in range(100, 401, 4)))")
 for tau in $taus; do
     echo "Running tau=$tau..."
     python3 square_wave_single_tau.py "$tau"
